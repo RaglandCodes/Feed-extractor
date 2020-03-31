8bc1b4084427fafc6ad1580e2a00779668b046ce
raglandcodes <raglandasir@gmail.com>
2020-03-31 16:53:18 +0530

    Set up Webpack and TS

--- src/handler.ts
+++ src/handler.ts
@@ -1,12 +1,15 @@
 export async function handleRequest(request: Request): Promise<Response> {
+  console.log('TTTTTTTTTTTTTTT')
+
   console.log(`${JSON.stringify(request, null, 2)} <== request`)
 
   console.log(`${request.url} <== request.url`)
   let link = request.url.split('=')
+  let a: number = 788
 
   console.log(`${link[1]} <== link22`)
 
-  return new Response(JSON.stringify({ OK: 'OK' }), {
+  return new Response(JSON.stringify({ OK: 'again OK 200' }), {
     headers: {
       'content-type': 'application/json',
       'Access-Control-Allow-Origin': '*',
--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,41 @@
 const path = require('path')
 const webpack = require('webpack')
 
-const mode = process.env.NODE_ENV || 'production'
+//const mode = process.env.NODE_ENV || 'production'
 
-module.exports = {
-  output: {
-    filename: `index.js`,
-    path: path.join(__dirname, 'build'),
-  },
-  devtool: 'source-map',
-  mode,
-  watch: true,
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js'],
-    plugins: [],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        loader: 'ts-loader',
-        options: {
-          transpileOnly: true,
+module.exports = (env, argv) => {
+  const mode = argv ? argv.mode : process.env.NODE_ENV
+
+  //console.log(`${JSON.stringify(argv, null, 2)} <== argv`)
+  //console.log(`${process.env.NODE_ENV} <== process.env.NODE_ENV`)
+  let config = {
+    output: {
+      filename: `index.js`,
+      path: path.join(__dirname, 'build'),
+    },
+    devtool: 'source-map',
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js'],
+      plugins: [],
+    },
+    mode: mode,
+    watch: mode === 'development',
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          loader: 'ts-loader',
+          exclude: /node_modules/,
+          options: {
+            transpileOnly: mode === 'production',
+            configFile: 'tsconfig.json',
+          },
         },
-      },
-      { enforce: 'pre', test: /\.js$/, loader: 'source-map-loader' },
-    ],
-  },
+        { enforce: 'pre', test: /\.js$/, loader: 'source-map-loader' },
+      ],
+    },
+    stats: mode === 'development' ? 'errors-warnings' : 'verbose',
+  }
+
+  return config
 }
